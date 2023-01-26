3e38f7da7313c3326d50bb3011fa920cf3463123
QuanBao-dev <quy123bao20@gmail.com>
2023-01-25 22:27:08 -0800

    done

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ patchModel.watch().on("change", (data) => {
       const document = data.fullDocument;
       const { title, id } = document.dataVN;
       announcementChannel.send(`
-        New Machine Translation has been published.\n**${title}**\nhttps://sugoivisualnovel.up.railway.app/vns/${id}\nIf you want to say thanks, you can ${supportMeChannelString}\n@everyone`);
+        New Machine Translation has been published.\n**${title}**\nhttps://sugoivisualnovel.up.railway.app/vns/${id}\nIf you want to encourage me to do more, you can ${supportMeChannelString}\n@everyone`);
       break;
     default:
       break;
